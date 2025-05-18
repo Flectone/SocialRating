666e1db171e6aff9e9903aaa92fbe9d095f8ce35
= <bakinu9@gmail.com>
2025-05-19 00:27:30 +0300

    add config for events when player rating is too low

--- src/main/java/org/bakinu/socialRating/service/Config.java
+++ src/main/java/org/bakinu/socialRating/service/Config.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import net.elytrium.serializer.SerializerConfig;
 import net.elytrium.serializer.language.object.YamlSerializable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class Config extends YamlSerializable {
     private static final SerializerConfig CONFIG = new SerializerConfig.Builder().build();
@@ -12,4 +15,50 @@ public class Config extends YamlSerializable {
         super(Config.CONFIG);
     }
 
+    private Command command = new Command();
+    private LowRatingEvents lowRatingEvents = new LowRatingEvents();
+
+    @Getter
+    public static final class Command {
+        private Command.GetRating getrating = new Command.GetRating();
+        private Command.SetRating setrating = new Command.SetRating();
+        private Command.SrReload srreload = new Command.SrReload();
+
+        @Getter
+        public static final class GetRating {
+            private String message = "<color:#cb52ff>Rating of <color:#e0a2fa>{player_name}</color>: <color:#e0a2fa>{rating}";
+        }
+        @Getter
+        public static final class SetRating {
+            private String message = "<color:#cb52ff>Rating of <color:#e0a2fa>{player_name}</color> setted to <color:#e0a2fa>{value}</color>, new rating: <color:#e0a2fa>{rating}";
+        }
+        @Getter
+        public static final class SrReload {
+            private String message = "<color:#cb52ff>Plugin reloaded!";
+        }
+    }
+
+    @Getter
+    public static final class LowRatingEvents {
+        private MuteChat muteChat = new MuteChat();
+
+        @Getter
+        public static final class MuteChat {
+            private boolean enabled = false;
+            private int rating = 90;
+            private String message = "<red>You can't speak because you'r rating is too low";
+        }
+
+        private MuteCommands muteCommands = new MuteCommands();
+
+        @Getter
+        public static final class MuteCommands {
+            private boolean enabled = false;
+            private int rating = 90;
+            private String message = "";
+            private List<String> blockedCommands = new ArrayList<>(List.of(
+                    "/me"
+            ));
+        }
+    }
 }

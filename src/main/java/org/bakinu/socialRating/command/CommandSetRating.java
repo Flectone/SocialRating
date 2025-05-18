8f0e02abc3ce95328462e3a5a004dd352ce1c461
= <bakinu9@gmail.com>
2025-05-19 00:28:00 +0300

    add message on command set rating

--- src/main/java/org/bakinu/socialRating/command/CommandSetRating.java
+++ src/main/java/org/bakinu/socialRating/command/CommandSetRating.java
@@ -1,19 +1,24 @@
 package org.bakinu.socialRating.command;
 
-import org.apache.commons.lang3.StringUtils;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bakinu.socialRating.database.UserDAO;
+import org.bakinu.socialRating.service.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandSetRating implements CommandExecutor {
     private final UserDAO userDAO;
-    public CommandSetRating(UserDAO userDAO) {
+    private final Config config;
+    private final MiniMessage miniMessage;
+
+    public CommandSetRating(UserDAO userDAO, Config config, MiniMessage miniMessage) {
         this.userDAO = userDAO;
+        this.config = config;
+        this.miniMessage = miniMessage;
     }
 
     @Override
@@ -30,12 +35,18 @@ public class CommandSetRating implements CommandExecutor {
         try {
             integer = Integer.parseInt(value);
         } catch (NumberFormatException e) {
-            e.printStackTrace();
             return false;
         }
 
         int finalRating = userDAO.getRating(player) + integer;
         userDAO.update(player, finalRating);
+
+        String message = config.getCommand().getSetrating().getMessage()
+                .replace("{player_name}", player.getName())
+                .replace("{value}", value)
+                .replace("{rating}", String.valueOf(finalRating));
+
+        commandSender.sendMessage(miniMessage.deserialize(message));
 
         return true;
     }
